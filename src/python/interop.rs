14bf2e695df841c1b70f7de5fcb4d6a08390fd2d
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-01-01 22:38:35 +0000

    [WIP] new interpreter state machine

--- src/python/interop.rs
+++ src/python/interop.rs
@@ -8,13 +8,14 @@ pub fn turnip_text(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<BlockNode>()?;
     m.add_class::<InlineNode>()?;
     m.add_class::<UnescapedText>()?;
+    m.add_class::<RawText>()?;
     m.add_class::<Sentence>()?;
     m.add_class::<Paragraph>()?;
 
     // Scopes
-    m.add_class::<ScopeOwner>()?;
-    m.add_class::<ImplicitScopedBlock>()?;
-    m.add_class::<ExplicitScopedBlock>()?;
+    m.add_class::<BlockScopeOwner>()?;
+    m.add_class::<BlockScope>()?;
+    m.add_class::<InlineScopeOwner>()?;
     m.add_class::<InlineScope>()?;
 
     Ok(())
@@ -28,55 +29,68 @@ fn experiment() -> PyResult<usize> {
 
 /// Parent class for block elements within the document tree e.g. paragraphs, block scopes.
 #[pyclass(subclass)]
-struct BlockNode {}
+pub struct BlockNode {}
+#[pymethods]
+impl BlockNode {
+    #[new]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
 /// Parent class for objects representing content that stays within a single line/sentence.
 #[pyclass(subclass)]
-struct InlineNode {}
+pub struct InlineNode {}
+#[pymethods]
+impl InlineNode {
+    #[new]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
 /// Represents plain inline text that has not yet been "escaped" for rendering.
 ///
 /// Typically created by Rust while parsing input files.
 #[pyclass(extends=InlineNode, subclass)]
-struct UnescapedText(Py<PyString>);
+pub struct UnescapedText(Py<PyString>);
+#[pymethods]
+impl UnescapedText {
+    #[new]
+    pub fn new(data: Py<PyString>) -> (Self, InlineNode) {
+        (Self(data), InlineNode::new())
+    }
+}
 
 /// A sequence of [InlineNode] that represents a single sentence.
 ///
 /// Typically created by Rust while parsing input files.
 #[pyclass(subclass)]
-struct Sentence(Vec<Py<InlineNode>>);
+pub struct Sentence(Vec<Py<InlineNode>>);
 
 /// A sequence of [Sentence] that combine to make a complete paragraph.
 ///
 /// Typically created by Rust while parsing input files.
 #[pyclass(extends=BlockNode, subclass)]
-struct Paragraph(Vec<Py<Sentence>>);
+pub struct Paragraph(Vec<Py<Sentence>>);
 
 /// Represents a block of plain text that may contain newlines (TODO are newlines normalized to \n?)
 ///
 /// Typically created by Rust while parsing input files.
-#[pyclass(extends=BlockNode, subclass)]
-struct RawTextBlock(Py<PyString>);
+#[pyclass(extends=InlineNode, subclass)]
+pub struct RawText(Py<PyString>);
+#[pymethods]
+impl RawText {
+    #[new]
+    pub fn new(data: Py<PyString>) -> (Self, InlineNode) {
+        (Self(data), InlineNode::new())
+    }
+}
 
 /// A parent class (subclassed in Python) representing the "owner" of a scope,
 /// which may modify how that scope is rendered.
 #[pyclass(subclass)]
-struct ScopeOwner {}
-
-/// A block of [Paragraph]s and other [BlockNode]s, owned by a [ScopeOwner].
-///
-/// Implicitly created e.g. `[section("sectionref")]{Section Title}` automatically encompasses all following text until the next implicit block.
-#[pyclass(extends=BlockNode, subclass)]
-struct ImplicitScopedBlock {
-    owner: Py<ScopeOwner>, // `[section("sectionref")]{Section Title}`
-    children: Vec<Py<BlockNode>>,
-    /// Used to determine what happens when another implicit block is encountered.
-    /// If `self.level < next.level`, `next` is made a child of `self`.
-    /// Otherwise it is considered the end of this implicit block, and it works up the stack (until the next explicit block).
-    ///
-    /// ex. `chapter.level = 0`, `section.level = 10`, `subsection.level = 20`... work it out from there.
-    level: i64,
-}
+pub struct BlockScopeOwner {}
 
 /// A block of [Paragraph]s and other [BlockNode]s, owned by a [ScopeOwner].
 ///
@@ -88,16 +102,33 @@ struct ImplicitScopedBlock {
 ///     paragraph 2
 /// }```
 #[pyclass(extends=BlockNode, subclass)]
-struct ExplicitScopedBlock {
-    owner: Option<Py<ScopeOwner>>,
+pub struct BlockScope {
+    owner: Option<Py<BlockScopeOwner>>,
     children: Vec<Py<BlockNode>>,
 }
+#[pymethods]
+impl BlockScope {
+    #[new]
+    pub fn new() -> (Self, BlockNode) {
+        (Self { owner: None, children: vec![] }, BlockNode::new())
+    }
+}
+impl Default for BlockScope {
+    fn default() -> Self {
+        BlockScope { owner: None, children: vec![] }
+    }
+}
+
+/// A parent class (subclassed in Python) representing the "owner" of a scope,
+/// which may modify how that scope is rendered.
+#[pyclass(subclass)]
+pub struct InlineScopeOwner {}
 
 /// A sequence of [UnescapedText]s and other [InlineNode]s, owned by a [ScopeOwner].
 ///
 /// e.g. `[code]{this_is_formatted_as_code}`
 #[pyclass(extends=InlineNode, subclass)]
-struct InlineScope {
-    owner: Option<Py<ScopeOwner>>,
+pub struct InlineScope {
+    owner: Option<Py<InlineScopeOwner>>,
     children: Vec<Py<InlineNode>>,
 }
