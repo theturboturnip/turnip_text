0dc8abb35a41f4f575fd5d12fc031564837cc7a0
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-03-17 14:50:42 +0000

    Move to Python module
    Instead of embedding a Python interpreter in Rust,
    build the Rust as an extension module for Python.
    Includes allowances to make testing work, which still requires embedding
    Python in Rust.

--- src/python/mod.rs
+++ src/python/mod.rs
@@ -3,14 +3,18 @@ use pyo3::{prelude::*, types::PyDict};
 use crate::lexer::TTToken;
 
 pub mod interop;
-use interop::turnip_text;
 
 mod interp;
 use self::{interop::BlockScope, interp::InterpState};
 
 pub mod typeclass;
 
+/// Prepare an embedded Python interpreter with our module.
+/// 
+/// Not valid when included as an extension module, but used when running Rust directly e.g. in testing.
+#[cfg(not(feature="extension-module"))]
 pub fn prepare_freethreaded_turniptext_python() {
+    use interop::turnip_text;
     pyo3::append_to_inittab!(turnip_text);
     pyo3::prepare_freethreaded_python();
 }
