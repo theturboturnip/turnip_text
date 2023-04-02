995e2f528764c92a03f70173751c3b5fb96bb330
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-02 23:22:01 +0100

    [WIP] Experiments with plugin-based rendering
    Drew out a generic-enough setup for adding plugins to renderers.
    Plugins seem cool because A) the dictify setup lends itself really well
    to combining different classes into one environment
    and B) it allows specific "rendering state" to be modular.
    The current Renderer protocol already seems bloated.

--- python/turnip_text/renderers/__init__.py
+++ python/turnip_text/renderers/__init__.py
@@ -1,7 +1,9 @@
+import abc
 from os import PathLike
-from typing import Any, Callable, Dict, List, Mapping, Optional, Protocol, Tuple, TypeVar, Union, runtime_checkable
+from typing import Any, Callable, Dict, Generic, Iterable, Iterator, List, Mapping, Optional, Protocol, Tuple, Type, TypeVar, Union, runtime_checkable
 
-from turnip_text import BlockScope, BlockScopeBuilder, Inline, InlineScopeBuilder, parse_file_native
+from turnip_text import Block, BlockScope, BlockScopeBuilder, Inline, InlineScopeBuilder, parse_file_native
+from turnip_text.turnip_text import InlineScope, Paragraph, Sentence, UnescapedText
 
 CiteKey = str
 CiteKeyWithNote = Tuple[CiteKey, str]
@@ -110,3 +112,101 @@ class Renderer(Protocol):
     def subsubsection(self, name: str, label: Optional[str] = None, numbered: bool = False) -> BlockScopeBuilder: ...
 
     # Figures?
+
+T = TypeVar('T')
+CustomRenderFunc = Callable[['ExperimentalActualRenderer', T], str]
+class TypeToRenderMap(Generic[T], abc.ABC):
+    handlers: Dict[Type[T], CustomRenderFunc]
+    default_render: CustomRenderFunc
+
+    def __init__(self, default_render: CustomRenderFunc) -> None:
+        super().__init__()
+        self.handlers = {}
+        self.default_render = default_render
+
+    def push_association(self, t: Type[T], func: CustomRenderFunc):
+        self.handlers[t] = func
+
+    def render(self, base_renderer: 'ExperimentalActualRenderer', obj: T) -> str:
+        for t, renderer in self.handlers.items():
+            if isinstance(obj, t):
+                return renderer(base_renderer, obj)
+        return self.default_render(base_renderer, obj)
+
+class ExperimentalActualRenderer(abc.ABC):
+    PARAGRAPH_SEP: str
+    SENTENCE_SEP: str
+
+    plugins: List['ExperimentalRendererPlugin']
+
+    block_handlers: TypeToRenderMap[Block]
+    inline_handlers: TypeToRenderMap[Inline]
+
+    def __init__(self, plugins: List['ExperimentalRendererPlugin']) -> None:
+        super().__init__()
+
+        # Create handlers and 
+        self.block_handlers = TypeToRenderMap(lambda _, __: raise NotImplementedError("Need default block handler?"))
+        self.block_handlers.push_association(BlockScope, lambda _, bs: self.render_blockscope(bs))
+        self.block_handlers.push_association(Paragraph, lambda _, bs: self.render_paragraph(bs))
+
+        self.inline_handlers = TypeToRenderMap(lambda _, __: raise NotImplementedError("Need default inline handler?"))
+        self.inline_handlers.push_association(InlineScope, lambda _, inls: self.render_inlinescope(inls))
+        self.inline_handlers.push_association(UnescapedText, lambda _, t: self.render_unescapedtext(t))
+
+        self.plugins = plugins
+        for p in self.plugins:
+            for (t, func) in p.block_handlers():
+                self.block_handlers.push_association(t, func)
+            for (t, func) in p.inline_handlers():
+                self.inline_handlers.push_association(t, func)
+
+    def render_unescapedtext(self, t: UnescapedText) -> str:
+        """The baseline - take text and return a string that will look like that text exactly in the given backend."""
+        raise NotImplementedError(f"Need to implement render_unescapedtext")
+    
+    def render_doc(self, doc: BlockScope) -> str:
+        # TODO: Document prefix/postfix?
+        return self.render_blockscope(doc)
+
+    def render_inline(self, i: Inline) -> str:
+        return self.inline_handlers.render(self, i)
+    
+    def render_block(self, b: Block) -> str:
+        return self.block_handlers.render(self, b)
+    
+    def render_blockscope(self, bs: BlockScope) -> str:
+        # Default: join paragraphs with self.PARAGRAPH_SEP
+        # If you get nested blockscopes, this will still be fine - you won't get double separators
+        return self.PARAGRAPH_SEP.join(
+            self.render_block(b)
+            for b in bs
+        )
+    
+    def render_paragraph(self, p: Paragraph) -> str:
+        # Default: join sentences with self.SENTENCE_SEP
+        return self.SENTENCE_SEP.join(self.render_sentence(s) for s in p)
+
+    def render_inlinescope(self, inls: InlineScope) -> str:
+        # Default: join internal inline elements directly
+        return "".join(
+            self.render_inline(i)
+            for i in inls
+        )
+    
+    def render_sentence(self, s: Sentence) -> str:
+        # Default: join internal inline elements directly
+        # TODO could be extended by e.g. latex to ensure you get sentence-break-whitespace at the end of each sentence?
+        return "".join(
+            self.render_inline(i)
+            for i in s
+        )
+
+class ExperimentalRendererPlugin:
+    name: str
+
+    def block_handlers(self) -> Iterable[Tuple[Type[Block], CustomRenderFunc]]:
+        return ()
+    def inline_handlers(self) -> Iterable[Tuple[Type[Inline], CustomRenderFunc]]:
+        return ()
+    
