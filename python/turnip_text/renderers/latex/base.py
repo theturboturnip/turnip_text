f603ed3bc86d59bcdc9ab7cdf8c2f9dcfd96b244
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-03 22:46:59 +0100

    Latex text escaping

--- python/turnip_text/renderers/latex/base.py
+++ python/turnip_text/renderers/latex/base.py
@@ -20,5 +20,21 @@ class LatexRenderer(Renderer):
         return r.text
 
     def render_unescapedtext(self, t: UnescapedText) -> str:
-        ### TODO!
-        return t.text
+        # note - right now this assumes we're using a unicode-compatible setup and thus don't need to escape unicode characters.
+        # note - order is important here because the subsitutions may introduce more special characters.
+        # e.g. if the backslash replacement applied later, the backslash from escaping "%" would be replaced with \textbackslash
+        ascii_map = {
+            "\\": "\\textbackslash{}",
+            "%": "\%",
+            "$": "\$",
+            "{": "\{",
+            "}": "\}",
+            "_": "\_",
+            "#": "\#",
+            "&": "\&",
+            "~": "\~{}",
+        }
+        data = t.text
+        for c, replace_with in ascii_map.items():
+            data = data.replace(c, replace_with)
+        return data
