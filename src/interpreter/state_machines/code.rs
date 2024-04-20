c8517e7d3e2ce330bbe5da53ce2806aedee9cf9a
Samuel Stark <samuel.winslow.stark@gmail.com>
2024-04-20 21:02:30 +0100

    Move eval_or_exec into the code state machine, because it doesn't need to be called from anywhere else

--- src/interpreter/state_machines/code.rs
+++ src/interpreter/state_machines/code.rs
@@ -1,13 +1,12 @@
 use std::{cell::RefCell, rc::Rc};
 
-use pyo3::{prelude::*, types::PyDict};
+use pyo3::{exceptions::PySyntaxError, ffi::Py_None, intern, prelude::*, types::PyDict};
 
 use crate::{
     error::TurnipTextContextlessResult,
     interpreter::{InterpError, MapContextlessResult},
     lexer::TTToken,
     python::{
-        eval_or_exec,
         interop::{
             coerce_to_inline_pytcref, BlockScopeBuilder, BuilderOutcome, InlineScopeBuilder,
             RawScopeBuilder, TurnipTextSource,
@@ -219,5 +218,51 @@ impl BuildFromTokens for CodeFromTokens {
             ))),
             BuilderOutcome::None => Ok(BuildStatus::Done(None)),
         }
+    }
+}
+
+pub fn eval_or_exec<'py, 'code>(
+    py: Python<'py>,
+    py_env: &'py PyDict,
+    code: &'code str,
+) -> PyResult<&'py PyAny> {
+    // Python picks up leading whitespace as an incorrect indent.
+    let code = code.trim();
+    // In exec() contexts it would be really nice to allow a toplevel indent (ignoring blank lines when calculating it)
+    // This requires two steps: determining if there is an indent, and removing that indent.
+    // Detecting an indent could be done programmatically *if* PyO3 exposed PyIndentError like it does PySyntaxError,
+    // otherwise we'd have to do our own parsing. The branch manual-indent-injection has some initial regexes for this,
+    // but I realized that doing this detection also requires ignoring comments (they don't count towards indentation)
+    // and I thought that strays too close to actually parsing the python for my liking.
+    // Resolving the indent is another problem: a feature automatically "dedenting" code passed to python -c
+    // exists https://discuss.python.org/t/allowing-indented-code-for-c/44122/1 and proposes appending `if True:` to the start as a stopgap.
+    // textwrap.dedent() also exists but I don't think it's suitable for code.
+    // Thus I have abandoned this quest.
+    let raw_res = match py.eval(code, Some(py_env), None) {
+        Ok(raw_res) => raw_res,
+        Err(error) if error.is_instance_of::<PySyntaxError>(py) => {
+            // Try to exec() it as a statement instead of eval() it as an expression
+            py.run(code, Some(py_env), None)?;
+            // Acquire a Py<PyAny> to Python None, then use into_ref() to convert it into a &PyAny.
+            // This should optimize down to `*Py_None()` because Py<T> and PyAny both boil down to *ffi::Py_Object;
+            // This is so that places that *require* non-None (e.g. NeedBlockBuilder) will always raise an error in the following match statement.
+            // This is safe because Py_None() returns a pointer-to-static.
+            unsafe { Py::<PyAny>::from_borrowed_ptr(py, Py_None()).into_ref(py) }
+        }
+        Err(error) => return Err(error),
+    };
+    // If it has __get__, call it.
+    // `property` objects and other data descriptors use this.
+    let getter = intern!(py, "__get__");
+    if raw_res.hasattr(getter)? {
+        // https://docs.python.org/3.8/howto/descriptor.html
+        // "For objects, the machinery is in object.__getattribute__() which transforms b.x into type(b).__dict__['x'].__get__(b, type(b))."
+        //
+        // We're transforming `[x]` into (effectively) `py_env.x`
+        // which should transform into (type(py_env).__dict__['x']).__get__(py_env, type(py_env))
+        // = raw_res.__get__(py_env, type(py_env))
+        raw_res.call_method1(getter, (py_env, py_env.get_type()))
+    } else {
+        Ok(raw_res)
     }
 }
