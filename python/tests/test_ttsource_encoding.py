7b5ad920ab85f7a14244dac3292de0d9ab7ca2ab
Samuel Stark <samuel.winslow.stark@gmail.com>
2024-05-02 22:57:16 +0100

    Try to test more nonstandard file encodings

--- python/tests/test_ttsource_encoding.py
+++ python/tests/test_ttsource_encoding.py
@@ -26,6 +26,65 @@ Y \[ˈʏpsilɔn\], Yen \[jɛn\], Yoga \[ˈjoːgɑ\]
         ),
         segments=[],
     )
-    print(expected_doc)
-    print(doc)
+    assert expected_doc == doc
+
+
+# Even after putting the text through UTF-16, turnip-text should still handle it
+def test_utf16():
+    original_src = r"""
+ði ıntəˈnæʃənəl fəˈnɛtık əsoʊsiˈeıʃn
+Y \[ˈʏpsilɔn\], Yen \[jɛn\], Yoga \[ˈjoːgɑ\]
+"""
+    utf8 = io.StringIO(
+        initial_value=str(original_src.encode(encoding="utf-16be"), encoding="utf-16be")
+    )
+    src = TurnipTextSource.from_file("<test>", utf8)
+    doc = parse_file_native(src, {})
+    expected_doc = Document(
+        contents=BlockScope(
+            [
+                Paragraph(
+                    [
+                        Sentence([Text("ði ıntəˈnæʃənəl fəˈnɛtık əsoʊsiˈeıʃn")]),
+                        Sentence([Text("Y [ˈʏpsilɔn], Yen [jɛn], Yoga [ˈjoːgɑ]")]),
+                    ]
+                )
+            ]
+        ),
+        segments=[],
+    )
+    assert expected_doc == doc
+
+
+# Even after putting the text through SHIFT-JIS, turnip-text should still handle it
+def test_shift_jis():
+    original_src = r"""
+A｡｢｣､･ｦｧｨｩｪｫｬｭｮｯ
+Bｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿ
+Cﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏ
+Dﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ
+"""
+    utf8 = io.StringIO(
+        initial_value=str(
+            original_src.encode(encoding="shift-jis"), encoding="shift-jis"
+        )
+    )
+    src = TurnipTextSource.from_file("<test>", utf8)
+    doc = parse_file_native(src, {})
+    # Even comparing the doc to one generated from UTF-8 should work
+    expected_doc = Document(
+        contents=BlockScope(
+            [
+                Paragraph(
+                    [
+                        Sentence([Text("A｡｢｣､･ｦｧｨｩｪｫｬｭｮｯ")]),
+                        Sentence([Text("Bｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿ")]),
+                        Sentence([Text("Cﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏ")]),
+                        Sentence([Text("Dﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ")]),
+                    ]
+                )
+            ]
+        ),
+        segments=[],
+    )
     assert expected_doc == doc
