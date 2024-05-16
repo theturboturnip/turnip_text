d94a6e1f3a4b37276fe0bcfcaaa7eadbb4c3e1fc
Samuel Stark <samuel.winslow.stark@gmail.com>
2024-05-16 19:38:41 +0100

    Upgrade pandoc library to handle newer pandoc versions

--- python/turnip_text/render/pandoc/pandoc_types.py
+++ python/turnip_text/render/pandoc/pandoc_types.py
@@ -2,7 +2,7 @@
 The `pandoc` Python package autogenerates a bunch of types from Haskell.
 For ease of use with mypy, I have written a script that generates type hints for the given version of the package.
 I import the types through this package to get those type hints.
-Currently it's for version 2.3 I believe.
+Currently it's for version 2.4 I believe.
 """
 
 from pandoc.types import *  # type: ignore
